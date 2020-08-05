19911af16165a866a4570f828e8e451483f4fc48
Artem Kudrin <akudrin@netlabsystems.com>
2020-08-05 21:23:55 +0500

    tried to split logic for presenter

---
+++ app/src/main/java/com/example/forecast/Presenter/Interfaces/IWeatherPresenter.kt
@@ -0,0 +1,11 @@
+package com.example.forecast.Presenter.Interfaces
+
+import com.example.forecast.Model.WeatherModel
+
+
+interface IWeatherPresenter {
+
+    fun fetchData()
+
+//    fun getWeather()
+}
---
+++ app/src/main/java/com/example/forecast/Presenter/WeatherPresenter.kt
@@ -0,0 +1,35 @@
+package com.example.forecast.Presenter
+
+import com.example.forecast.Model.WeatherModel
+import com.example.forecast.Presenter.Interfaces.IWeatherPresenter
+import com.example.forecast.Presenter.Retrofit.IWeatherRequest
+import com.example.forecast.Presenter.Retrofit.RetrofitClient
+import com.example.forecast.View.IWeatherView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class WeatherPresenter(val viewInterface: IWeatherView): IWeatherPresenter {
+
+    val CITY = "yekaterinburg, ru"
+    val UNITS = "metric"
+    val KEY = "4d8f0b60c4e68439a8707fb6e0878962"
+
+    val retrofit = RetrofitClient.instance
+    val getWeatherApi = retrofit.create(IWeatherRequest::class.java)
+
+    override fun fetchData() {
+
+        val compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(getWeatherApi.getCurrentWeather(CITY, UNITS, KEY)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { viewInterface.onLoading() }
+            .doFinally { viewInterface.onLoaded() }
+            .subscribe(
+                { weather -> viewInterface.displayWeather(weather)},
+                { viewInterface.onError(t = it)}
+            )
+        )
+    }
+}
